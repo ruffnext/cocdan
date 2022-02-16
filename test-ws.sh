6f516cea673dccda3e70b0aff993c4f9c359ad95
ruffnext <ruffnext@protonmail.com>
2022-02-16 09:24:54 +0800

    clean project

--- test-ws.sh
+++
@@ -1,7 +0,0 @@
-curl    --include \
-        --no-buffer \
-        --header "Connection: Upgrade" \
-        --header "Upgrade: websocket" \
-        --header "Host: localhost:3000" \
-        --cookie "ring-session=863adeba-aa54-4f9a-8f5d-b8570f0067b6;JSESSIONID=ebDZS9PdLlXjKENhvQUC0lzmctJka4FuvjB32eQi" \
-        http://localhost:3000/ws/1
